b46f3cda91ed2323485a0fe1463d99b3f7818acb
MC-MOOGLE <dmitriybosoy@gmail.com>
2026-04-27 14:32:14 +0300

    add image

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ if (form && message && messageText) {
     const palette = formData.get("palette");
     const email = formData.get("email");
 
-    messageText.textContent = "\u041e\u0442\u0432\u0435\u0442 \u0434\u043b\u044f " + email + " \u043f\u0440\u0438\u043d\u044f\u0442. \u0412\u0430\u0448 \u0432\u044b\u0431\u043e\u0440: " + palette + ".";
+    messageText.textContent = "Ответ " + email + " принят. Ваш выбор (не осуждаю): " + palette + ".";
     message.hidden = false;
     form.reset();
   });
